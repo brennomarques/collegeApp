f2d357d164dcb962649923fb4ee611318c158089
brenno.lima <brenno.lima@soluti.com.br>
2021-08-25 22:54:18 -0300

    Criando componente textfield

--- lib/app/pages/user_data_from.dart
+++ lib/app/pages/user_data_from.dart
@@ -1,3 +1,4 @@
+import 'package:college/app/shared/components/Custom_Text_Field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +10,53 @@ class UserDate extends StatefulWidget {
 }
 
 class _UserDateState extends State<UserDate> {
+  final controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dados pesssoais'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Aqui e Dados pessoais')],
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(
+                  label: 'Número de matrícula',
+                  icon: Icons.confirmation_num,
+                  keyboard: TextInputType.number,
+                ),
+                SizedBox(height: 15),
+                CustomTextField(
+                  label: 'Nome completo',
+                  icon: Icons.person,
+                  keyboard: TextInputType.name,
+                ),
+                SizedBox(height: 15),
+                CustomTextField(
+                  label: 'Nome da faculdade',
+                  icon: Icons.home,
+                  keyboard: TextInputType.name,
+                ),
+                SizedBox(height: 15),
+                CustomTextField(label: 'Teste icon'),
+                SizedBox(height: 15),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.save),
+                    label: Text("Salvar"),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
