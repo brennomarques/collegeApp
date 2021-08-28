494dbd4113bfb43e84efc6ffefe1367c970cfc3b
brenno.lima <brenno.lima@soluti.com.br>
2021-08-27 22:40:52 -0300

    componentizando text field

--- lib/app/pages/user_data_from.dart
+++ lib/app/pages/user_data_from.dart
@@ -1,6 +1,7 @@
-import 'package:college/app/shared/components/Custom_Text_Field.dart';
+import 'package:college/app/shared/components/custom_text_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:string_validator/string_validator.dart' as validator;
 
 class UserDate extends StatefulWidget {
   // const UserDate({ Key? key }) : super(key: key);
@@ -10,7 +11,7 @@ class UserDate extends StatefulWidget {
 }
 
 class _UserDateState extends State<UserDate> {
-  final controller = TextEditingController();
+  final formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class _UserDateState extends State<UserDate> {
       ),
       body: SingleChildScrollView(
         child: Form(
+          key: formkey,
           child: Padding(
             padding: EdgeInsets.all(20),
             child: Column(
@@ -29,31 +31,50 @@ class _UserDateState extends State<UserDate> {
                   label: 'Número de matrícula',
                   icon: Icons.confirmation_num,
                   keyboard: TextInputType.number,
+                  validator: (text) => text == null || text.isEmpty
+                      ? 'O campo matricula e obrigatório'
+                      : null,
                 ),
                 SizedBox(height: 15),
                 CustomTextField(
-                  label: 'Nome completo',
-                  icon: Icons.person,
-                  keyboard: TextInputType.name,
-                ),
+                    label: 'Nome completo',
+                    icon: Icons.person,
+                    keyboard: TextInputType.name,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'O nome completo e obrigatório';
+                      }
+                      return null;
+                    }),
                 SizedBox(height: 15),
                 CustomTextField(
-                  label: 'Nome da faculdade',
-                  icon: Icons.home,
-                  keyboard: TextInputType.name,
-                ),
-                SizedBox(height: 15),
-                CustomTextField(label: 'Teste icon'),
+                    label: 'Nome da faculdade',
+                    icon: Icons.home,
+                    keyboard: TextInputType.name,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return 'O nome da faculdade e obrigatório';
+                      }
+                      return null;
+                    }),
                 SizedBox(height: 15),
                 SizedBox(
                   width: double.infinity,
                   height: 50,
                   child: ElevatedButton.icon(
-                    onPressed: () {},
+                    onPressed: () {
+                      formkey.currentState?.validate();
+                      if (formkey.currentState.validate()) {
+                        formkey.currentState.save();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processando dados')),
+                        );
+                      }
+                    },
                     icon: Icon(Icons.save),
                     label: Text("Salvar"),
                   ),
-                )
+                ),
               ],
             ),
           ),
